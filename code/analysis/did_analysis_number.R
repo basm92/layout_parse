76c6199c311356b54d411bc60b189a73e057d035
Bas Machielsen <basmachielsen@live.nl>
2024-02-15 14:01:41 +0100

    add table and regression generation functions

--- code/analysis/did_analysis_number.R
+++ code/analysis/did_analysis_number.R
@@ -5,6 +5,8 @@ library(fixest)
 library(sf) 
 library(marginaleffects)
 library(modelsummary)
+library(knitr)
+library(kableExtra)
 
 # Regression settings
 source('./code/analysis/regression_settings.R')
@@ -20,16 +22,7 @@ formula_did <- as.formula(
   )
 
 model_lin <- feols(formula_did, data = dataset, vcov=vcov_conley)
-treatment_test_lin <- marginaleffects::hypotheses(
-  model=model,
-  hypothesis="b1=b6",
-  vcov=vcov_conley)
-
 model_pois <- fepois(formula_did, data = dataset, vcov = vcov_conley)
-treatment_test_pois <- marginaleffects::hypotheses(
-  model=model2,
-  hypothesis="b1=b6",
-  vcov=vcov_conley)
 
 
 # Table
@@ -37,7 +30,8 @@ notes <- "Dependent variables: Number of innovations in municipality $i$.
 The coefficient of interest is the Year x Group{Veneto} variable.
 The control variables are latitude, longitude, elevation, and the 
 analysis is conditional on provinde fixed-effects. Standard errors are 
-estimated using Conley (1999)."
+estimated using Conley (1999). The F Statistic and p-value report the outcome of
+a test for the equality $\\beta_1=\\beta_6$." 
 notes <- gsub('[\n]', ' ', notes) |> str_squish()
 
 rows <- tribble(~term,         ~"(1)", ~"(2)",
@@ -48,15 +42,16 @@ knitr::opts_current$set(label = "did_analysis_number")
 modelsummary(
   list(model_lin,
        model_pois),
+  stars=stars,
+  gof_map = gm_did, 
+  coef_map = cm_did,
   title = "Difference-in-difference Estimates of Unification on Innovation",
   add_rows = rows,
-  stars=stars,
   coef_omit = "Intercept",
-  gof_map = gm,
   out = "kableExtra",
-  output = "latex") #  |>
-  # kableExtra::add_header_above(c(" " = 1, "Single Municipality" = 4, "Municipality + Surroundings" = 4)) |>
+  output = "latex")   |>
+  kableExtra::add_header_above(c(" " = 1, "OLS" = 1, "Poisson" = 1)) |>
   kableExtra::kable_styling(latex_options = c("hold_position", "scale_down")) |>
   kableExtra::footnote(general = notes, footnote_as_chunk = T, threeparttable = T, escape = F) |>
-  kableExtra::save_kable("./tables/iv_analysis_share_bankinglandscape_pc.tex")
+  kableExtra::save_kable(file="./tables/did_analysis_number.tex")
 
