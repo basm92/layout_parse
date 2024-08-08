0452a2b64a2f4d1bbcd56ce11c1be63584fb4bff
Bas Machielsen <basmachielsen@live.nl>
2024-08-08 18:26:44 +0200

    add class information

--- code/data_wrangling/data_wrangling_exhibitions.R
+++ code/data_wrangling/data_wrangling_exhibitions.R
@@ -15,6 +15,13 @@ together <- list_rbind(imported_files) |>
 
 # Load the class boundaries
 source('./data/dictionaries/class_boundaries.R')
+class_boundary <- class_boundary |>
+  mutate(year = as.character(year))
+# Add the classes to the together dataset
+together <- together |>
+  left_join(class_boundary, by = c('year', 'page', 'position_on_page')) |>
+  fill(class)
+
 
 # And two separate exhibition data from 1855 and 1911
 # These are individual-level datasets but lack description of exhibition/name
@@ -22,7 +29,7 @@ exh_1911 <- read_xlsx('./data/raw_exhibition_data/1911_italy.xlsx') |>
   clean_names() |>
   filter(country_1 == "Italia") |>
   select(location_1, class_number, nuvas_abridged_class) |>
-  rename(location = location_1, class = class_number) |>
+  rename(location = location_1, class = class_number, rca_class = nuvas_abridged_class) |>
   mutate(year = "1911")
 
 exh_1855 <- read_xlsx('./data/raw_exhibition_data/Exhibitions_Lombardo_Veneto_1855.xlsx') |>
@@ -30,4 +37,32 @@ exh_1855 <- read_xlsx('./data/raw_exhibition_data/Exhibitions_Lombardo_Veneto_18
   select(-seqn) |>
   mutate(year = as.character(year))
 
-together_1855_1911 <- bind_rows(exh_1911, exh_1855)
+together_1855_1911 <- bind_rows(exh_1911, exh_1855) |>
+  rename(place = location)
+
+# Add the 1855-1911 to the rest of the data
+final <- together |>
+  bind_rows(together_1855_1911)
+
+# Add the complexity index
+complexity <- read_delim('./data/raw_outcomes_data/complexity.csv') |>
+  mutate(year = as.character(year))
+
+individual_with_class <- final |>
+  left_join(complexity, by = c('year', 'class')) |>
+  mutate(class_description = if_else(is.na(rca_class.x), rca_class.y, rca_class.x)) |>
+  select(-c(rca_class.x, rca_class.y))
+
+# Save this as an intermediate-level exhibition dataset
+write_csv2(individual_with_class, "./data/intermediate_exhibition_data/individual_level_exhibition_data.csv")
+
+# Geocode this data
+
+
+# Aggregate the data to the (year, city, count) level and use geocode
+aggregated_data <- individual_with_class |>
+  group_by(year, place) |>
+  summarize(count = n(), average_complexity = mean(pci, na.rm=T), top_complexity = max(pci))
+
+
+
