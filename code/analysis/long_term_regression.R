e3931b16dfbc04ff7d9f29f619550e1ab97f8e42
Bas Machielsen <basmachielsen@live.nl>
2024-09-05 13:07:24 +0200

    delete some irrelevant stuff - first attempt at analysis

--- code/analysis/long_term_regression.R
+++ code/analysis/long_term_regression.R
@@ -1,5 +1,6 @@
 # Long Run: Patents and Exhibitions
-library(fixest); library(tidyverse); library(did)
+library(fixest); library(tidyverse); library(did); library(didimputation)
+source("./code/data_wrangling/data_wrangling_final_ds.R")
 ## Estimator: Staggered DiD - Use the Borusyak-Jaravel estimator or Callaway-Sant'Anna
 ## DV: Log(PatentCount_{Italy, Austria}), 
 ## Part 1: Patents: {1855-1867-1878-1890-1902-1911}
@@ -10,4 +11,22 @@ library(fixest); library(tidyverse); library(did)
 ## DV: Log(ExhibitionCount), AvgComplexity, TopComplexity
 ## Weights: Stuff by Population
 ## Robustness: Extensive vs. Intensive Margin; Unit of Measurement
+final <- final |>
+  mutate(treated = if_else((year > 1855 & allegiance_1861 == "Lombardia") | (year > 1867 & allegiance_1861 == "Veneto"), 1, 0),
+         treatment = case_when(allegiance_1861 == "Lombardia" ~ 1867, 
+                               allegiance_1861 == "Veneto"~ 1878, 
+                               TRUE ~ NA))
 
+feols(log(1+count) ~ 1 + i(year, ref=1855) + i(year, treated) | PRO_COM, 
+      data = final ,
+      vcov=~DEN_CIRC)
+
+did::att_gt(yname="count", 
+            gname="treatment",
+            idname="PRO_COM",
+            tname="year",
+            xformla=~1,
+            data=final,
+            est_method="reg",
+            control_group = "notyettreated"
+            )
