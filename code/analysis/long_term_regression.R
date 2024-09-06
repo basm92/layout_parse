b21fb75aeb6e39f0f70d367fa26dd17c6f2b4260
Bas Machielsen <basmachielsen@live.nl>
2024-09-06 22:24:03 +0200

    add first part long term reg

--- code/analysis/long_term_regression.R
+++ code/analysis/long_term_regression.R
@@ -29,7 +29,39 @@ patents1902 <- feols(patents_together*1e4 ~ allegiance_1861 + area_of_intersecti
                      weights=~1/abs(running),
                      vcov=~DEN_PROV)
 
-modelsummary(list(patents1855, patents1867, patents1878, patents1889, patents1902), stars=T)
+coef_map <- c("allegiance_1861Veneto"="Veneto")
+
+# Table notes
+n <- "Table reports estimates of the difference in patent count in Veneto relative to Lombardy, 
+focusing on 5 years for which patent counts are available: 1855, 1867, 1878, 1889, and 1900. 
+The coefficient size is multiplied by 1000 for interpretability. 
+The coefficients are estimated by OLS with a bandwidth of 100 kilometer, and are conducted at the \\textit{Comune} level. 
+The estimates are weighted by the inverse absolute distance to the border, and control for area and distance to the border, 
+and are also conditional on year fixed-effects. Heteroskedasticity-robust standard errors are clustered at the province-level. $*: p<0.1, **: p<0.05, ***: p<0.01$."
+
+
+panels <- list(
+  '1855'=patents1855, 
+  '1867'=patents1867, 
+  '1878'=patents1878,
+  '1889'=patents1889,
+  '1902'=patents1902)
+
+modelsummary(panels,
+             coef_map=coef_map,
+             stars=c("*"=0.1, "**"=0.05, "***"=0.01),
+             gof_map = tibble(raw=c("adj.r.squared", "nobs"), 
+                              clean=c("Adj. $R^2$", "N"),
+                              fmt=c(3, 0)),
+             title="Estimates of Unification on Long-term Patenting Activity\\label{tab:patent}",
+             estimate = "{estimate}{stars}",
+             notes = n, 
+             output = "tinytable",
+             width=c(0.20, 0.15, 0.15, 0.15, 0.15, 0.15)) |>
+  style_tt(
+    i=1, bold=T) |>
+  save_tt("./tables/patents_long_term.tex", overwrite = T)
+
 
 ## Part 2: Exhibitions: {1855-1867-1878-1889-1900-1911}
 ## DV: Log(ExhibitionCount), AvgComplexity, TopComplexity
